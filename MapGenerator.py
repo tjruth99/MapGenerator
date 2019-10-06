82054a7b3b9d83d8ba333f80f4cb81235f647c7d
Tyler Ruth <tyler1hero@sbcglobal.net>
2019-10-06 00:17:02 -0400

    Added console inputs and new function to print map

--- MapGenerator.py
+++ MapGenerator.py
@@ -2,15 +2,23 @@ import random
 from colorama import init, Back
 init()
 
-n = 10
-steps = 100
+n = int(input("enter n: "))
+steps = int(input("enter steps: "))
 map = [[0 for i in range(n)] for j in range(n)] 
+
+def printmap():
+    for i in range(n):
+        for j in range(n):
+            if map[i][j] == 0:
+                print(Back.BLUE + "0", end = " ")
+            else:
+                print(Back.GREEN + "1", end = " ")
+        print()
+    pass
 
 def randomWalk():
     x = int(n/2)
     y = int(n/2)
-
-    print(x,y)
 
     for i in range(steps):
         if (0 < x < n) and (0 < y < n):
@@ -26,13 +34,7 @@ def randomWalk():
         else:
             y = y - 1
 
-    for i in range(n):
-        for j in range(n):
-            if map[i][j] == 0:
-                print(Back.BLUE + "0", end = " ")
-            else:
-                print(Back.GREEN + "1", end = " ")
-        print()
+    printmap()
     pass
 
 randomWalk()
