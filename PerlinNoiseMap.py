53bf135ca6222f46527c5216d762fc339b61a67d
Tyler Ruth <tyler1hero@sbcglobal.net>
2019-10-11 15:09:49 -0400

    added scalable elevations

--- PerlinNoiseMap.py
+++ PerlinNoiseMap.py
@@ -7,6 +7,7 @@ deepWater = [0, 0, 150]
 water = [0, 0, 225]
 sand = [194, 178, 128]
 grass = [0, 150, 0]
+forest = [0, 100, 0]
 mountain = [100, 100, 100]
 snow = [225, 225, 225]
 
@@ -18,18 +19,18 @@ lacunarity = 2.0
 
 def generateNoiseMap():
     print("Generate Perlin Noise Map")
-    #help(noise)
     map = numpy.zeros((n,n))
     data = numpy.zeros( (n,n,3), dtype=numpy.uint8 )
     
+    # TODO: FInd a better way to do variability
     seed = int(input("seed: "))
 
     random.seed( seed )
 
     rand = random.randint(0, 1024)
 
-    max = -1
-    min = 1
+    max = -100
+    min = 100
 
     for i in range(n):
         for j in range(n):
@@ -40,7 +41,7 @@ def generateNoiseMap():
                                     lacunarity, 
                                     repeatx=n, 
                                     repeaty=n, 
-                                    base=rand) * 10
+                                    base=rand) + 0.5
             map[i][j] = num
 
             if num > max:
@@ -48,22 +49,39 @@ def generateNoiseMap():
 
             if num < min:
                 min = num
-                
-            if( num < -1 ):                 # deep ocean
+
+    colorMap(map,max,min)
+
+    generateNoiseMap()
+    pass
+
+def colorMap(map, max, min):
+    data = numpy.zeros( (n,n,3), dtype=numpy.uint8 )
+
+    deepOceanLevel = (max-min)*0.4
+    oceanLevel = (max-min)*0.5
+    sandLevel = (max-min)*0.55
+    landLevel = (max-min)*0.62
+    forestLevel = (max-min)*0.7
+    mountainLevel = (max-min)*0.85
+
+    for i in range(n):
+        for j in range(n):
+            num = map[i][j]
+            if( num < deepOceanLevel ):     # deep ocean
                 data[i][j] = deepWater
-            elif ( num < 0 ):               # ocean
+            elif ( num < oceanLevel ):      # ocean
                 data[i][j] = water
-            elif ( num < 0.5 ):             # sand
+            elif ( num < sandLevel ):       # sand
                 data[i][j] = sand
-            elif ( num < 1.5 ):             # land
+            elif ( num < landLevel):        # land
                 data[i][j] = grass
-            elif( num < 2.0 ):              # mountains
+            elif ( num < forestLevel ):     # forest
+                data[i][j] = forest
+            elif( num < mountainLevel ):    # mountains
                 data[i][j] = mountain
             else:                           #snow
                 data[i][j] = snow
 
     Image.fromarray(data).show()
-    print(max,min)
 
-    generateNoiseMap()
-    pass
