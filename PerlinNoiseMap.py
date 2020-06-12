3814a1615a1fee6fda8262c89951860e6e0e5154
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-06-11 20:50:20 -0400

    edited implementation of perlin noise map
    can more easily change color levels

--- PerlinNoiseMap.py
+++ PerlinNoiseMap.py
@@ -3,6 +3,7 @@ import numpy
 import random
 from PIL import Image
 
+# RGB Colors of map
 deepWater = [0, 0, 150]
 water = [0, 0, 225]
 sand = [194, 178, 128]
@@ -11,11 +12,32 @@ forest = [0, 100, 0]
 mountain = [100, 100, 100]
 snow = [225, 225, 225]
 
+# Determines what color a point is based on its percent of 
+colorLevels = [0.35, 0.4, 0.45, 0.6, 0.7, 0.85]
+
 n = 1024
-scale = 100.0
-octaves = 5
+scale = 200
+octaves = 7
 persistence = 0.5
 lacunarity = 2.0
+
+def getMinMax(map):
+    n = int(map.size**(1/2.0))
+    max = -1024
+    min = 1024
+
+    for i in range(n):
+        for j in range(n):
+            num = map[i][j]
+
+            if num > max:
+                max = num
+
+            if num < min:
+                min = num
+
+    return [min, max]
+
 
 def generateNoiseMap():
     print("Generate Perlin Noise Map")
@@ -24,36 +46,18 @@ def generateNoiseMap():
     # TODO: Find a better way to do variability
     seed = int(input("seed: "))
 
-    random.seed( seed )
-
-    rand = random.randint(0,1024)
-
-    print(rand)
-
-    max = -1024
-    min = 1024
-
     for i in range(n):
         for j in range(n):
-            num = noise.snoise2(    (i/scale), 
+            map[i][j] = noise.snoise2((i/scale), 
                                     (j/scale), 
                                     octaves, 
                                     persistence, 
                                     lacunarity, 
                                     repeatx=n, 
                                     repeaty=n, 
-                                    base=rand) + 0.5
-            map[i][j] = num
+                                    base=seed) + 0.5
 
-            if num > max:
-                max = num
-
-            if num < min:
-                min = num
-
-    print(min, max)
-    #greyscale(map)
-    colorMap(map,max,min)
+    colorMap(map)
 
     return map
 
@@ -67,15 +71,19 @@ def greyscale(map):
 
     Image.fromarray(data).show()
 
-def colorMap(map, max, min):
+def colorMap(map):
     data = numpy.zeros( (n,n,3), dtype=numpy.uint8 )
 
-    deepOceanLevel = (max-min)*0.35 + min
-    oceanLevel = (max-min)*0.4 + min 
-    sandLevel = (max-min)*0.45 + min
-    landLevel = (max-min)*0.60 + min
-    forestLevel = (max-min)*0.7 + min
-    mountainLevel = (max-min)*0.85 + min
+    mm = getMinMax(map)
+    min = mm[0]
+    max = mm[1]
+
+    deepOceanLevel = (max-min)*colorLevels[0]+min
+    oceanLevel = (max-min)*colorLevels[1]+min
+    sandLevel = (max-min)*colorLevels[2]+min
+    landLevel = (max-min)*colorLevels[3]+min
+    forestLevel = (max-min)*colorLevels[4]+min
+    mountainLevel = (max-min)*colorLevels[5]+min
 
     for i in range(n):
         for j in range(n):
@@ -97,24 +105,17 @@ def colorMap(map, max, min):
 
     Image.fromarray(data).show()
 
+# Combining two perlin noise maps tends to give maps with a lot of islands
 def combine():
     map1 = generateNoiseMap()
     map2 = generateNoiseMap()
     map3 = numpy.zeros((n,n))
 
-    max = -1024
-    min = 1024
-
     for i in range(n):
         for j in range(n):
             num = map1[i][j] * map2[i][j]
             map3[i][j] = num
-            if num > max:
-                max = num
 
-            if num < min:
-                min = num
-
-    colorMap(map3, max, min)
-
+    colorMap(map3)
+    
 generateNoiseMap()
