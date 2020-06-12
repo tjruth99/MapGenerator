22b9400383b8f3fa2f13a0a75a4940894be64a90
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-06-11 20:04:11 -0400

    use Primm's algorithm to draw a better path between rooms

--- Dungeon.py
+++ Dungeon.py
@@ -100,7 +100,7 @@ def generateDungeon(n, cells):
     minRoomSizeDim = 10
     
     # stores the start and end coordinates of each room
-    rooms=[]
+    rooms = []
 
     # ignore some rooms to give dungeon a more varied look, not always a grid
     ignoreRoomPercent = 25
@@ -124,34 +124,8 @@ def generateDungeon(n, cells):
                     for y in range(lenY):
                         map[int(x + startX)][int(y + startY)] = 1
 
-    # add a linear path to each room
-    for i in range(1, len(rooms)):
-        fromRoom = rooms[i-1]
-        toRoom = rooms[i]
-
-        # get the starting coordinates for the hallway from somewhere in the from room
-        startX = random.randint(fromRoom[0]+1, fromRoom[2]-1)
-        startY = random.randint(fromRoom[1]+1, fromRoom[3]-1)
-
-        # get the ending coordinates for the hallway from somewhere in the to room
-        endX = random.randint(toRoom[0]+1, toRoom[2]-1)
-        endY = random.randint(toRoom[1]+1, toRoom[3]-1)
-
-        # draw the hallway on the x plane
-        if(endX > startX):
-            for j in range(startX, endX):
-                map[j][startY] = 1
-        else: 
-            for j in range(endX, startX):
-                map[j][startY] = 1
-
-        # draw the hallway on the y plane
-        if(endY > startY):
-            for j in range(startY, endY):
-                map[endX-1][j] = 1
-        else: 
-            for j in range(endY, startY):
-                map[endX-1][j] = 1
+    # Draw a path connecting each room
+    drawPath(map, rooms)
 
     # draw a border on the edge to make the map look cleaner
     for i in range(n):
@@ -163,6 +137,92 @@ def generateDungeon(n, cells):
     printColor(map)
 
     return map
+
+# Draw the path to each room using minimum spanning tree
+def drawPath(map, rooms):
+    connectedRooms = [0]
+
+    # Use Primm's Algorithm to draw a connected path to each room
+    for i in range(len(rooms)-1):
+        minNode = []
+        minValue = 999999
+
+        # Store a list of the edges that connects nodes already in the tree
+        listOfEdges = []
+        for i in connectedRooms:
+            distances = getDistanceToEachRoom(i, rooms)
+            for j in distances:
+                listOfEdges.append(j)
+
+        # Go through each edge to find the minimum 
+        for j in listOfEdges:
+            # Check to make sure the node does not connect two rooms already inside the list
+            if(not (j[1] in connectedRooms and j[2] in connectedRooms)):
+                # Get the minimum value
+                if(j[0] < minValue):
+                    minValue = j[0]
+                    minNode = j
+
+        # Append the indexes of the rooms added to the list of already connected rooms
+        connectedRooms.append(minNode[1])
+        connectedRooms.append(minNode[2])
+
+        # Draw the hallway between the rooms
+        drawHallway(map, rooms[minNode[1]], rooms[minNode[2]])
+    pass
+
+# Take the index of a room and give a list of the distances to each of the other rooms in the dungeon
+def getDistanceToEachRoom(index, rooms):
+    curRoom = rooms[index]
+
+    # Use the center of the current room to measure the distance
+    curX = int((curRoom[0] + curRoom[2]) / 2)
+    curY = int((curRoom[1] + curRoom[3]) / 2)
+
+    distances = []
+
+    for i in range(len(rooms)):
+        targetRoom = rooms[i]
+
+        # Use the center of the targer room to measure the distance
+        targetX = int((targetRoom[0] + targetRoom[2]) / 2)
+        targetY = int((targetRoom[1] + targetRoom[3]) / 2)
+
+        # Get the distance
+        d= int(math.sqrt((targetX - curX)**2 + (targetY - curY)**2))
+
+        # Ignore distances of 0
+        if(i != index):
+            # Append an array of the distance, the current room index, and the targer room index
+            distances.append([d, index, i])
+
+    return distances
+
+# Draw a hallway between two rooms
+def drawHallway(map, fromRoom, toRoom):
+    # get the starting coordinates for the hallway from somewhere in the from room
+    startX = random.randint(fromRoom[0]+1, fromRoom[2]-1)
+    startY = random.randint(fromRoom[1]+1, fromRoom[3]-1)
+
+    # get the ending coordinates for the hallway from somewhere in the to room
+    endX = random.randint(toRoom[0]+1, toRoom[2]-1)
+    endY = random.randint(toRoom[1]+1, toRoom[3]-1)
+
+    # draw the hallway on the x plane
+    if(endX > startX):
+        for j in range(startX, endX):
+            map[j][startY] = 1
+    else: 
+        for j in range(endX, startX):
+            map[j][startY] = 1
+
+    # draw the hallway on the y plane
+    if(endY > startY):
+        for j in range(startY, endY):
+            map[endX-1][j] = 1
+    else: 
+        for j in range(endY, startY):
+            map[endX-1][j] = 1
 
             
 def printColor(map):
@@ -200,6 +260,7 @@ def printArray(map):
         for j in range(n):
             print(map[i][j], end=" ")
         print()
+
 
 generateDungeon(90, 3)
 #generateClassicDungeon(10, 10)
