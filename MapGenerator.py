66aae1cccf525a165bcdd7c6d66d3d63435a2188
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-06-13 02:04:23 -0400

    added random walk map to gui

--- MapGenerator.py
+++ MapGenerator.py
@@ -3,15 +3,16 @@ from tkinter import messagebox
 from tkinter import ttk
 from PIL import ImageTk, Image
 
-from PerlinNoiseMap import generateNoiseMap
 from Dungeon import generateDungeon
+from PerlinNoiseMap import generateNoiseMap
+from RandomWalk import generateRandomWalkMap
 
 imgSize = 500,500
 cells = 3
 
 def getDungeon(): 
     try:
-        cells = int(e.get())
+        cells = int(dungeonSize.get())
     except:
         messagebox.showwarning("NaN", "Enter a number into the box!")
         return
@@ -30,37 +31,52 @@ def getPerlinMap():
     canvas.create_image(0,0, anchor=NW, image=img)
     canvas.image = img
 
+def getRandomWalkMap():
+    map = generateRandomWalkMap(250, 50000, 0, 0, 0, 2)
+    img = ImageTk.PhotoImage(map.resize(imgSize))
+    canvas.create_image(0,0, anchor=NW, image=img)
+    canvas.image = img
+
+
 if __name__ == "__main__":
     root = Tk()
     root.title("Map Generator")
     root.resizable(False, False)
+    root.grid_columnconfigure(0, weight=1)
 
-    notebook = ttk.Notebook(root)
+    notebook = ttk.Notebook(root, width=500)
 
-    dungeonFrame = Frame(notebook, width=501, height=600)
-    dungeonFrame.grid(row=0,column=0, sticky="N")
+    dungeonFrame = Frame(notebook)
+    dungeonFrame.grid(row=0, column=0, sticky="N")
+    dungeonFrame.columnconfigure(0, weight=1)
 
-    perlinFrame = Frame(notebook, width=501, height=600)
-    perlinFrame.grid(row=0,column=1, sticky="N")
+    perlinFrame = Frame(notebook)
+    perlinFrame.grid(row=0, column=1, sticky="N")
+    perlinFrame.columnconfigure(0, weight=1)
+
+    walkFrame = Frame(notebook)
+    walkFrame.grid(row=0, column=1, sticky="N")
+    walkFrame.columnconfigure(0, weight=1)
 
     notebook.add(dungeonFrame, text="Dungeon")
     notebook.add(perlinFrame, text="Perlin Map")
-    notebook.grid(row=0,column=0)
+    notebook.add(walkFrame, text="Random Walk")
+    notebook.grid(row=0, column=0)
     
     Label(dungeonFrame, text="Dungeon Generator").grid(row=0,column=0)
-
     Label(dungeonFrame, text="Size of dungeon (2-15)").grid(row=1, column=0)
-    e = Entry(dungeonFrame, width=10)
-    e.insert(0, "3")
-    e.grid(row=2,column=0)
-
+    dungeonSize = Entry(dungeonFrame, width=10)
+    dungeonSize.insert(0, "3")
+    dungeonSize.grid(row=2, column=0)
     Button(dungeonFrame, text="Generate Dungeon", command=getDungeon).grid(row=3,column=0)
 
     Label(perlinFrame, text="Perlin Noise Map Generator").grid(row=0,column=0)
-
     Button(perlinFrame, text="Generate Map", command=getPerlinMap).grid(row=1,column=0)
 
+    Label(walkFrame, text="Random Walk Map Generator").grid(row=0,column=0)
+    Button(walkFrame, text="Generate Map", command=getRandomWalkMap).grid(row=1,column=0)
+
     canvas = Canvas(root, width=500, height=500)
-    canvas.grid(row=4,column=0)
+    canvas.grid(row=4, column=0)
 
     root.mainloop()
--- debug.py
+++ debug.py
@@ -2,9 +2,9 @@ from Dungeon import *
 from PerlinNoiseMap import *
 from RandomWalk import *
 
-generateRandomWalkMap(100, 10000, 16, 61, 0, 2).resize((500,500)).show()
+generateRandomWalkMap(250, 50000, 0, 0, 0, 2).resize((500,500)).show()
 
-cells = int(input("Dungeon Size: "))
-img = generateDungeon(cells)
-img.resize((500,500)).show()
+#cells = int(input("Dungeon Size: "))
+#img = generateDungeon(cells)
+#img.resize((500,500)).show()
 
